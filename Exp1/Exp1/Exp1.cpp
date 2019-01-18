98cb0b037ca624c8542ea2cf00f209ddde14c745
Snow <polarpenguin@foxmail.com>
2019-01-18 18:24:12 +0800

    NextFit Mem Alloc Policy

--- Exp1/Exp1/Exp1.cpp
+++ Exp1/Exp1/Exp1.cpp
@@ -1,3 +1,5 @@
+// Exp1.cpp
+// Snow 2019-01-18
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
@@ -41,7 +43,57 @@ FREEBLOCK_TABLE BlockTable;
 
 RESULT NextFitAlloc(unsigned Size)
 {
-	return SUCCESS;
+	static FREEBLOCKTABLE_NODE *pCurNode;
+	if (pCurNode == NULL)
+	{
+		pCurNode = BlockTable.Head->Next;
+	}
+	FREEBLOCKTABLE_NODE *pNodeTag = pCurNode;
+	unsigned LoopTag = 1;
+	
+	while (1)
+	{
+		if (pCurNode == NULL)
+		{
+			if (LoopTag == 1)
+			{
+				pCurNode = BlockTable.Head->Next;
+				LoopTag = 0;
+				continue;
+			}
+			else
+				break;
+		}
+		else if (pCurNode == pNodeTag && LoopTag == 0)
+		{
+			break;
+		}
+
+		if (pCurNode->MemBlock.State == BLOCK_FREE
+			&& pCurNode->MemBlock.BlockSize == Size)
+		{
+			pCurNode->MemBlock.State = BLOCK_BUSY;
+			return SUCCESS;
+		}
+		else if (pCurNode->MemBlock.State == BLOCK_FREE
+			&& pCurNode->MemBlock.BlockSize > Size)
+		{
+			FREEBLOCKTABLE_NODE* TempNode = (FREEBLOCKTABLE_NODE*)malloc(sizeof(FREEBLOCKTABLE_NODE));
+			TempNode->MemBlock.BlockSize = Size;
+			TempNode->MemBlock.State = BLOCK_BUSY;
+			TempNode->Prev = pCurNode->Prev;
+			TempNode->Next = pCurNode;
+			TempNode->MemBlock.BlockAddress = pCurNode->MemBlock.BlockAddress;
+			pCurNode->Prev->Next = TempNode;
+			pCurNode->Prev = TempNode;
+			pCurNode->MemBlock.BlockAddress += Size;
+			pCurNode->MemBlock.BlockSize -= Size;
+			return SUCCESS;
+		}
+		pCurNode = pCurNode->Next;
+	}
+
+	return ERROR;
 }
 
 RESULT BestFitAlloc(unsigned Size)
@@ -131,10 +183,26 @@ int main()
 		{
 		case 1:
 			{
-				
+				unsigned RequestMemSize;
+				printf("分配内存大小(KB): ");
+				scanf("%u", &RequestMemSize);
+				if (LAlloc(RequestMemSize) == SUCCESS)
+				{
+					puts("分配成功");
+				}
+				else
+				{
+					puts("分配失败");
+				}
 			}
 			break;
 		case 2:
+			{
+				unsigned ReleaseMemAddr;
+				printf("释放内存块始地址: ");
+				scanf("%u", &ReleaseMemAddr);
+				LFree(ReleaseMemAddr);
+			}
 			break;
 		case 0:
 			return 0;
